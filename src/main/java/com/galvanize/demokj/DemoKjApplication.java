ce49ed9261f9eef9a597201f97a0d13693fe0dcf
anandmck <anand.kumar3@mckesson.com>
2020-02-04 09:00:20 -0600

    Initial commit

---
+++ src/main/java/com/galvanize/demokj/DemoKjApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.demokj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/demokj/DemoKjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.demokj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
